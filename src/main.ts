4d28363c6669877478290395b3046ccfd0d1de12
starsky <daihanzhang@gmail.com>
2025-09-05 13:48:26 +0800

    简化配置信息加载方式. 后续采用读取WebAPI方式加载. 先写死.

--- src/main.ts
+++ src/main.ts
@@ -8,30 +8,23 @@ import {configManager} from './configManager';
 import {createPinia} from 'pinia'
 
 async function initApp() {
-    try {
-        // 在应用启动时加载配置
-        console.log('Loading application configuration...');
-        const config = await configManager.loadConfig();
+    // 创建并挂载应用
+    const app = createApp(App)
+        .use(createPinia())
+        .use(router);
+    // 全局注册 Element Plus
+    app.use(ElementPlus)
+
+    // 在应用启动时加载配置
+    console.log('Loading application configuration...');
+    const config = await configManager.loadConfig();
+    if (config) {
         // 将配置设置为全局变量
         (window as any).APP_CONFIG = config;
-        // 创建并挂载应用
-        const app = createApp(App)
-            .use(createPinia())
-            .use(router);
-        // 全局注册 Element Plus
-        app.use(ElementPlus)
         // 也可以通过 provide 提供配置给 Vue 组件
         app.provide('appConfig', config);
-        app.mount('#app');
-    } catch (_) {
-        // 即使配置加载失败也启动应用
-        const app = createApp(App)
-            .use(createPinia())
-            .use(router);
-        // 全局注册 Element Plus
-        app.use(ElementPlus)
-        app.mount('#app');
     }
+    app.mount('#app');
 }
 
 // 启动应用
